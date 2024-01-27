3bd83a2e5a351ebbca1d23fc728590edf681fefd
Zimmermann <igor.sonstnix@gmail.com>
2024-01-27 21:00:07 +0100

    lesson05 was added

---
+++ src/main/java/lesson05/CleanCode.java
@@ -0,0 +1,30 @@
+package lesson05;
+
+public class CleanCode {
+
+    //Правила наименования переменных
+    //lowerCamelCase
+    int currentPage = 10;
+    String userName = "max";
+    String passwordGrad = "2323 eee 2i2";
+    //is, can, hat, isAvailable, canExecute.....
+    boolean hasChildren = true;
+
+    //Правила наименования методов
+    public int calculateTax(){
+        return 0;
+    }
+    public void printDocument(){
+        System.out.println("Document....");
+    }
+
+    public String findUserByUsername(){
+        return "max";
+    }
+
+    public void doIt(){
+
+    }
+
+
+}
