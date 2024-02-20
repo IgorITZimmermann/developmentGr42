9f2fe741d0304f316871474dc50a247b35778eb6
Zimmermann <igor.sonstnix@gmail.com>
2024-02-21 00:12:20 +0100

    lesson 11 was added

---
+++ src/main/java/lesson11/GetPersonDates.java
@@ -0,0 +1,14 @@
+package lesson11;
+
+import lesson11.Person;
+
+public class GetPersonDates {
+
+    public static void main(String[] args) {
+        Person person = new Person();
+        System.out.println(person.getName());
+        System.out.println(person.getColorOfEye());
+        System.out.println(person.getAge());
+
+    }
+}
