4f9d9c402467707d27b1e3bd85297f11332e9705
Igor Zimmermann <igor.sonstnix@gmail.com>
2025-03-25 00:43:14 +0100

    Implement JDBC-based authentication replacing in-memory setup

---
+++ DevelopmentGr42/src/main/java/de/ait/javalessons/utils/BcryptUtil.java
@@ -0,0 +1,15 @@
+package de.ait.javalessons.utils;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class BcryptUtil {
+    public static void main(String[] args) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String password = "adminpass";
+        String encodetPassword = encoder.encode(password);
+        System.out.println(password + "-->"+ encodetPassword);
+    }
+
+    //userpass-->$2a$10$XTYrfRQzPUDsJ.smAKwRougTNtZ80Ak267mGWnaYYQMZG9KaZD2jy
+    //adminpass-->$2a$10$pSM3dZIRXDX9qX2R94tF1ehbH6HcKGwApQJDwl3NmC8bvmlIbmqrK
+}
